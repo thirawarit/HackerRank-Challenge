7d152df9801849d9247d005882227f0d70300762
Thirawarit.p <Thirawarit.p@gmail.com>
2022-08-03 16:23:49 +0700

    Update SWAP-case.py

--- SWAP-case.py
+++ SWAP-case.py
@@ -1,12 +1,15 @@
 def swap_case(s):
     s = list(s)
     for i in range(len(s)) :
+        
         if s[i].isupper() :
             s[i] = s[i].lower()
         else :
             s[i] = s[i].upper()
     s = ''.join(s)
+    #s = s.swapcase() #>>>> "None",is str, cannot use for this*****
     return(s)
+
 
 if __name__ == '__main__':
     s = input()
