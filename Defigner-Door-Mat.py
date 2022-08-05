2a7728eb1ddfa459c1308475879eb6537fbd5498
Thirawarit.p <Thirawarit.p@gmail.com>
2022-08-05 15:40:10 +0700

    Update Defigner-Door-Mat.py

--- Defigner-Door-Mat.py
+++ Defigner-Door-Mat.py
@@ -1,7 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-row, columm = input().split()   #Input a string and split it to a list.Put each string to the variable.
-for n in range((int(row))//2) :      #Take loop n  by 0,1,2,...,int(row)
-    print(('.|.'*(2*n+1)).center(int(columm),'-'))
-print('WELCOME'.center(int(columm),'-'))
-for n in range((int(row))//2,0,-1) :    #Take reverse loop n by int(row), ..., 2, 1, 0
-    print(('.|.'*(2*n-1)).center(int(columm),'-'))
+row, columm = map(int,input().split())   #Input a string and split it to a list.Put each str to int by map() and take it the variable.
+for n in range(row//2) :      #Take loop n  by 0,1,2,...,int(row)
+    print(('.|.'*(2*n+1)).center(columm,'-'))
+print('WELCOME'.center(columm,'-'))
+for n in range(row//2,0,-1) :    #Take reverse loop n by int(row), ..., 2, 1, 0
+    print(('.|.'*(2*n-1)).center(columm,'-'))
