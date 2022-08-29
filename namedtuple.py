54865e098636cc95fa92918c359deafd0912e193
Thirawarit.p <Thirawarit.p@gmail.com>
2022-08-29 13:38:57 +0700

    Update namedtuple.py

--- namedtuple.py
+++ namedtuple.py
@@ -10,16 +10,18 @@ Constraints
 Output Format
 Print the average marks of the list corrected to 2 decimal places.
 ==============
-Sample Input
-TESTCASE 01
+---Sample Input---
+vvvvvvvvvvvvvv
+---TESTCASE 01---
 5
 ID         MARKS      NAME       CLASS     
 1          97         Raymond    7         
 2          50         Steven     4         
 3          91         Adrian     9         
 4          72         Stewart    5         
-5          80         Peter      6   
-TESTCASE 02
+5          80         Peter      6  
+===============
+---TESTCASE 02---
 5
 MARKS      CLASS      NAME       ID        
 92         2          Calum      1         
@@ -28,10 +30,10 @@ MARKS      CLASS      NAME       ID
 55         8          Glenn      4         
 82         2          Fergus     5
 ===============
-Sample Output
-TESTCASE 01
+---Sample Output---
+---TESTCASE 01---
 78.00
-TESTCASE 02
+---TESTCASE 02---
 81.00
 ===============
 Explanation
