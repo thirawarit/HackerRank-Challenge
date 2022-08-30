c31924ff43e27a3953b74f699cdd41de940ea3fa
Thirawarit.p <Thirawarit.p@gmail.com>
2022-08-30 11:26:44 +0700

    Create sale-by-match.py

---
+++ solve-problem/sale-by-match.py
@@ -0,0 +1,45 @@
+#sale by match
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    from collections import Counter
+    count = 0
+    socket = Counter(ar)
+    for i in set(ar):
+        if socket[i]%2 == 1:
+            count += (socket[i] - 1)//2
+        else:
+            count += socket[i]//2
+    return(count)
+
+            
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
