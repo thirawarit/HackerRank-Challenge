f9bd33b3e460751066457e43839e9ae5c506bcc4
Thirawarit.p <Thirawarit.p@gmail.com>
2022-10-15 01:25:25 +0700

    update

--- Practice-Myself/practice-class.py
+++ Practice-Myself/practice-class.py
@@ -1,15 +1,32 @@
-if '__main__' == __name__:
-    class TestSys:
-        attri_1 = int()
-        attri_2 = int()
-        def callname(self, name , year):
-            print("The name of the school is :", name, "that has been :", year, "years")
-        def schname():
-            print(TestSys.attri_1)
-
-    school = TestSys()
-    a = 'test'
-    school.callname('BEN', 120)
+#python class
+# Classes are created by keyword 'class'
+class Person:
     
-    TestSys.attri_1 = 200
-    TestSys.schname()
+    # vvvvvv --attributes
+    # Class Variable
+    attr1 = "Human"
+    attr2 = "Earth planet"
+    name = "Adam"
+    
+    # vvvvvv --methods
+    # 'class methods' must have an extra first parameter in the method definition
+    # init method or constructor
+    def __init__(self, name: str):
+        # Instance Variable
+        self.name = name
+    
+    # 'sample method'
+    def say_hi(self):
+        print('Hello, my name is', self.name)
+
+#Driver code
+#Object instantiation
+# when you create a 'declaring object', it consists of identity[name], state[attr], behaviour[method]
+
+# identity + state
+# we do not give a value for this parameter when we call the method, Python provides it
+p = Person('Nikhil')
+# behaviour
+p.say_hi()
+#accessing class attributes
+print(p.name)
